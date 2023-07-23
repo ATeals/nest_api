dca597dad19d3dbb8bcdf3918d337d5ca7b55c50
ateals <ateals@icloud.com>
2023-07-23 20:17:44 +0900

    cors setting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './Module/App/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000/', 'https://ateals.github.io/'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(8080, () => {
     console.log('✅ server run ' + 'http://localhost:8080/ ');
   });
